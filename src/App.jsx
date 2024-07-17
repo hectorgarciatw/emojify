786c00471bc8bac261fe29a3f3f8c2bf908abee5
Héctor <dreamallica@gmail.com>
2024-07-17 18:19:40 -0300

    Instalando Tailwind y Picnic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
 
+//Componentes
+import { Footer } from './components/Footer';
+
 function App() {
     return (
         <>
             <h1>Emojify</h1>
+            <Footer />
         </>
     );
 }
